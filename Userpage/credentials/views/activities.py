300302acc4860e1809e8d85e8a56ce292b021eb2
Jayanth <AXBXHIDE@TUTANOTA.COM>
2019-07-31 22:37:52 +0530

    Added activities and some UI

--- Userpage/credentials/views/activities.py
+++ Userpage/credentials/views/activities.py
@@ -1,25 +1,37 @@
 from django.shortcuts import render, redirect
-from credentials.models import Register, CodePost
 from django.contrib.auth.models import auth
 from django.contrib import messages
 from rest_framework.views import APIView
+from credentials.models import Register, CodePost, Comments
 
 
 class show_activities(APIView):
     def get(self, request):
         data = []
-        values = {}
         try:
             posts = CodePost.objects.all().order_by('-ts')
             for post in posts:
+                comments_list = []
+                coms = Comments.objects.filter(post_id = post.slno)
+                for com in coms:
+                    values = {
+                        'user': com.user,
+                        'comments': com.comments,
+                        'posted_time': com.ts,
+                        'updated_time': com.ets,
+                    }
+                    comments_list.append(values)
                 values = {
+                    'slno': post.slno,
                     'description': post.description,
                     'user': post.user,
                     'posted_time': post.ts,
                     'updated_time': post.ets,
                     'like_count': post.like_count,
                     'share_count': post.share_count,
+                    'comments': comments_list
                 }
+
                 data.append(values)
         except Exception as e:
             print(str(e))
@@ -28,15 +40,25 @@ class show_activities(APIView):
         return render(request, 'activity.html', {'rendered_data':data})
 
     def post(self, request):
+        print(request.POST['post_id'])
+        if request.POST['post_id'] == '':
+            print("I run now")
+            try:
+                post = CodePost()
+                post.user = str('Jayanth') + ' ' + str('Nagaraj')
+                post.description = request.POST['description']
+                # post.name = str(request.user.first_name) + ' ' + str(request.user.last_name)
+                post.save()
+            except Exception as e:
+                print(e)
 
-        try:
-            post = CodePost()
-            post.user = str('Jayanth') + ' ' + str('Nagaraj')
-            post.description = request.POST['job_description']
-            # post.name = str(request.user.first_name) + ' ' + str(request.user.last_name)
-            post.save()
-        except Exception as e:
-            print(e)
-            return render(request, 'activity.html')
+        elif request.POST['post_id']:
+            try:
+                comment = Comments()
+                comment.post_id = CodePost.objects.get(slno=request.POST['post_id'])
+                comment.user = str('Jayanth') + ' ' + str('Nagaraj')
+                comment.save()
+            except Exception as e:
+                print(e)
 
         return redirect('/activities')
